d33dfb805b91654797f36628c9397d0ea0e13edc
Krishna Shah <kshahusa@gmail.com>
2026-05-22 00:21:06 -0500

    clip v2

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -21,6 +21,19 @@ map("n", "<leader>e", function()
 end)
 
 vim.opt.clipboard = "unnamedplus"
+if os.getenv("SSH_TTY") then
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+    },
+  }
+end
 
 vim.keymap.set("n", "L", "<cmd>noh<CR><Esc>", { silent = true })
 
