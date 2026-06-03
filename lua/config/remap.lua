0c6405a607004ee1d2c92877af3b221fd49f24c4
Krishna Shah <kshahusa@gmail.com>
2026-06-02 19:48:06 -0500

    lin nums and formatter

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -1,46 +1,49 @@
 local map = vim.keymap.set
 
 map("n", "<leader>ff", function()
-  Snacks.picker.files()
+    Snacks.picker.files()
 end)
 
 map("n", "<leader>fg", function()
-  Snacks.picker.grep()
+    Snacks.picker.grep()
 end)
 
 map("n", "<leader>fb", function()
-  Snacks.picker.buffers()
+    Snacks.picker.buffers()
 end)
 
 map("n", "<leader>fh", function()
-  Snacks.picker.help()
+    Snacks.picker.help()
 end)
 
 map("n", "<leader>e", function()
-  Snacks.explorer()
+    Snacks.explorer()
 end)
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
 
 vim.opt.clipboard = "unnamedplus"
 if os.getenv("SSH_TTY") then
-  vim.g.clipboard = {
-    name = "OSC 52",
-    copy = {
-      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-    },
-    paste = {
-      ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-      ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-    },
-  }
+    vim.g.clipboard = {
+        name = "OSC 52",
+        copy = {
+            ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+        },
+        paste = {
+            ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+            ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+        },
+    }
 end
 
 vim.keymap.set("n", "L", "<cmd>noh<CR><Esc>", { silent = true })
 
 vim.keymap.set({ "n", "i", "v" }, "<C-f>", function()
-  require("conform").format({ async = true, lsp_fallback = true })
+    require("conform").format({ async = true, lsp_fallback = true })
 end, { desc = "Format buffer" })
 
 vim.keymap.set("n", "<leader>ft", function()
-  Snacks.picker.todo_comments()
+    Snacks.picker.todo_comments()
 end, { desc = "Todo Comments" })
--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,27 +1,35 @@
 vim.opt.showmode = false
 
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
 vim.o.autowriteall = true -- save on buffer leave, :next, etc.
 
 -- Save on idle (after 1 second of no typing)
 vim.api.nvim_create_autocmd({ "InsertLeave", "TextChanged" }, {
-	pattern = "*",
-	callback = function()
-		if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
-			vim.cmd("silent! write")
-		end
-	end,
+    pattern = "*",
+    callback = function()
+        if vim.bo.modified and vim.bo.buftype == "" and vim.fn.expand("%") ~= "" then
+            vim.cmd("silent! write")
+        end
+    end,
 })
 
 vim.api.nvim_create_autocmd("FocusLost", {
-	pattern = "*",
-	command = "silent! wa",
+    pattern = "*",
+    command = "silent! wa",
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-  callback = function(args)
-    local ok, parser = pcall(vim.treesitter.language.inspect, args.match)
-    if ok and parser then
-      vim.treesitter.start()
-    end
-  end,
+    callback = function(args)
+        local ok, parser = pcall(vim.treesitter.language.inspect, args.match)
+        if ok and parser then
+            vim.treesitter.start()
+        end
+    end,
 })
